9afadad2d34aba8c18a07dc6754bc7bb0d7eceb5
Ubuntu <ubuntu@ip-172-31-37-95.eu-west-1.compute.internal>
2020-10-19 14:31:22 +0000

    Component imported from react library. New constructor method added to enable new state creation. onClick method added to button to enable application of a text status change via use of this.setState. String changes upon button click from User.

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, my name is Paul.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class App extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      string: 'Hello, my name is Paul'
+    };
+  }
+  render() {
+    return (
+      <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>{ this.state.string }</p>
+        <button onClick={() => this.setState({ string: 'Hello Paul'})}>Change Text</button>
       </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
