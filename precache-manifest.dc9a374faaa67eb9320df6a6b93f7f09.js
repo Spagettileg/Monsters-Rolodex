e8010975021155a8b16792fc7b1f9546f3adee57
EC2 Default User <ec2-user@ip-172-31-44-212.eu-west-1.compute.internal>
2021-02-04 15:58:57 +0000

    Updates

---
+++ precache-manifest.dc9a374faaa67eb9320df6a6b93f7f09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f282ff90ee2cbaf0db1e8dcd5338c150",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9fbd2f89e0d25e4e9775",
+    "url": "/monsters-rolodex/static/css/main.49421161.chunk.css"
+  },
+  {
+    "revision": "0ac27ccecaca5553eea2",
+    "url": "/monsters-rolodex/static/js/2.640f7b8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.640f7b8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fbd2f89e0d25e4e9775",
+    "url": "/monsters-rolodex/static/js/main.bafeca3d.chunk.js"
+  },
+  {
+    "revision": "5d9871b43329dda10fc8",
+    "url": "/monsters-rolodex/static/js/runtime-main.98ead556.js"
+  }
+]);
